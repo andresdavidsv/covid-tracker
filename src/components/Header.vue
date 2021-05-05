418ef8e8731dc5bca46029c7935894b762a9b72b
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-05-04 20:37:02 -0500

    :heavy_plus_sign: Last Code, pre deploy

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
     <div class="text-3xl md:text-5xl font-bold mb-3">
-      Covid-19 Tracker
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
     </div>
     <p>
       API by
